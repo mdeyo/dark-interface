0d1d5ba0ede19cf042279895b025f5100af87337
Danielle Man <daniman0808@gmail.com>
2017-03-08 00:19:35 -0800

    update font + clock styles

--- main.js
+++ main.js
@@ -7,40 +7,46 @@ var jsonObject,jsonForecastObject;
 var weatherIcon, weatherTitle, weatherText, forecastText;
 
 function init() {
+  setInterval(() => {
+    $('#time').html(moment().format('hh:mm'));
+    $('#seconds').html(moment().format('ss'));
+    $('#tz').html(moment().format('a'));
+    $('#date').html(moment().format('MMMM Do, YYYY'));
+    $('#dow').html(moment().format('dddd'));
+  }, 1000);
 
-    weatherIcon = document.getElementById('weather-icon');
-    weatherTitle = document.getElementById('weather-title');
-    weatherText = document.getElementById('weather-text');
-    forecastText = document.getElementById('forecast-text');
+  weatherIcon = document.getElementById('weather-icon');
+  weatherTitle = document.getElementById('weather-title');
+  weatherText = document.getElementById('weather-text');
+  forecastText = document.getElementById('forecast-text');
 
-jQuery(document).ready(function($) {
-  $.ajax({
-  url : "http://api.wunderground.com/api/"+wundergroundKey+"/geolookup/conditions/q/CA/San_Francisco.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-	jsonObject = parsed_json;
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  var temp_feels_f = parsed_json['current_observation']['feelslike_f'];
-  var icon_url = parsed_json['current_observation']['icon_url'];
-  weatherIcon.src = icon_url;
-weatherTitle.innerHTML = "Current temperature in " + location;
-weatherText.innerHTML = temp_f + " F";
-
-  }
+  $(document).ready(function($) {
+    $.ajax({
+    url : "http://api.wunderground.com/api/"+wundergroundKey+"/geolookup/conditions/q/CA/San_Francisco.json",
+    dataType : "jsonp",
+    success : function(parsed_json) {
+    	jsonObject = parsed_json;
+      var location = parsed_json['location']['city'];
+      var temp_f = parsed_json['current_observation']['temp_f'];
+      var temp_feels_f = parsed_json['current_observation']['feelslike_f'];
+      var icon_url = parsed_json['current_observation']['icon_url'];
+      weatherIcon.src = icon_url;
+      weatherTitle.innerHTML = "Current temperature in " + location;
+      weatherText.innerHTML = temp_f + " F";
+      }
+    });
   });
-});
 
-jQuery(document).ready(function($) {
-  $.ajax({
-  url : "http://api.wunderground.com/api/"+wundergroundKey+"/geolookup/forecast/q/CA/San_Francisco.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-	jsonForecastObject = parsed_json;
-  	var high = parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'];
-  	var low = parsed_json['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'];
-	forecastText.innerHTML = 'Low: ' +low + 'F <br/> High: '+high+'F';
-
-  }
+  $(document).ready(function($) {
+    $.ajax({
+      url : "http://api.wunderground.com/api/"+wundergroundKey+"/geolookup/forecast/q/CA/San_Francisco.json",
+      dataType : "jsonp",
+      success : function(parsed_json) {
+      	jsonForecastObject = parsed_json;
+      	var high = parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'];
+      	var low = parsed_json['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'];
+      	forecastText.innerHTML = 'Low: ' +low + 'F <br/> High: '+high+'F';
+      }
+    });
   });
-});
+}
